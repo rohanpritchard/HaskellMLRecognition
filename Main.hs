65efdc30fc0d7a6cf1aa04e0a5d5284b591936af
Rohan Pritchard <rep15@ic.ac.uk>
2017-08-03 22:42:33 +0100

    Write compression function to reduce attribute values

--- Main.hs
+++ Main.hs
@@ -1,7 +1,15 @@
 import DTClassifier
 import Data.List.Split
 
-getData fp = do allInOne <- readFile fp
-                let dt = map (splitOn ",") (splitOn "\n" allInOne)
-                print $ dt
-                return ()
+getData fp header = do allInOne <- readFile fp
+                       let ds = stripAllSpaces $ map (splitOn ",") (splitOn "\n" allInOne)
+                       let dt = buildTree (header,ds) splitByEntropy
+                       return dt
+
+stripAllSpaces x = map (map strip) x
+
+strip x = dropWhile (==' ') x
+
+
+
+compress xs = (map (\x -> show (div (read x :: Int) 10)) (init xs)) ++ [(last xs)]
